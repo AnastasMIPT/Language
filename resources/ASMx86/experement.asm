225a1d8f81bdf61a2cf451b4fb8f7b265708e2d5
AnastasMIPT <AtosBel@ya.ru>
2020-08-19 15:36:16 +0300

    Correct data adresses in itoa, working output

--- resources/ASMx86/experement.asm
+++ resources/ASMx86/experement.asm
@@ -6,73 +6,13 @@ _start:
 		mov rax, 1           ; номер системного вызова  sys_exit
 		mov rbx, 0           ; код завершения программы
 		int 80h
-
-itoa:
-		push rbp
-		mov rbp, rsp
-		xor rax, rax
-		mov rax, qword [rbp+16]
-		mov rbx, qword number_rev
-		mov rdi, number_new
-		xor r10, r10
-		or rax, rax
-		jns .Loop
-		neg rax
-		mov byte [rdi], '-'
-		inc rbx
-		inc rdi
-.Loop:
-		xor rdx, rdx
-		mov r8, 0ah
-		div r8
-		add rdx, '0'
-		mov byte [rbx+r10], dl
-		inc r10
-		cmp qword r10, SYMB_POINT
-		jne .NoPoint
-		mov byte [rbx+r10], '.'
-		inc r10
-.NoPoint:
-		cmp rax, 0
-		je .Loop2
-		jmp .Loop
-.Loop2:		;writing reversev
-		dec r10
-		mov al, [rbx+r10]
-		stosb
-		cmp r10, 0
-		je .Exit
-		jmp .Loop2
-.Exit:
-
-		mov byte [rdi+1], 10
-		mov rsp, rbp
-		pop rbp
-		ret
-
-
-
-
-
-
-
-
 main:
 		push rbp
 		mov rbp, rsp
 
 	
 		mov rbx, qword 100
-		push rbx
-		call itoa
-		sub rsp, 8
-
-		mov rax, 4
-		mov rbx, 1
-		mov rcx, number_new
-		mov rdx, 11
-		int 80h
-
+		
 		mov rsp, rbp
 		pop rbp
 		ret
